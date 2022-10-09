6f95b7da2b4b52666f159738a7b2a90c63f8aeed
Kacper <kacper.mazur04@protonmail.com>
2022-10-09 12:23:47 +0200

    login_buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor()
+  {
+    super();
+    this.state={
+      email:"",
+      password:"",
+    }
+  }
+  submit(){
+    console.log(this.state);
+    fetch('url',{
+      method: 'post',
+      body:JSON.stringify(
+        this.state
+      ),
+      headers:{
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json',
+      }
+    }).then(function(response){
+      response.json().then(function(resp){
+        console.log(resp)
+      })
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Login dfafadfaf</h1>
+        <input type="text" onChange={(item)=>this.setState({email:item.target.value})}></input>
+        <input type="password" onChange={(item)=>this.setState({password:item.target.value})}></input>
+
+        <button onClick={()=>{this.submit()}}>Login</button>
+
+      </div>
+    );
+  }
 }
+
 
 export default App;
